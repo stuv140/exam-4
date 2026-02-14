9867ea1e987e25b38260e164f74e0b9e2928d667
stuv140 <stuv.otus@yandex.ru>
2026-02-14 17:35:02 +0300

    Create print_ip.cpp

---
+++ src/print_ip.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include<bitset>
+#include<stdint.h>
+#include<vector>
+#include<sstream>
+#include<list>
+#include<tuple>
+#include"Template.h"
+
+
+
+int main()
+{
+    int number = 2130706433;
+    int8_t u8 =  -1;
+    int16_t u16= 0;
+    int32_t u32 = 2130706433;
+    int64_t u64 = 8875824491850138409;
+    std::string str("Hello world!");
+    std::vector<int> vInt = {100,200,300,400};
+    std::list<short> lInt= {400,300,200,100};
+    auto make_tuple = if_make_types<int>(123, 456, 789, 0);
+    auto make_tuple2 = if_make_types<double>(123.3, 456.45, 789.666, 0.2);
+//    std::cout << std::bitset<32>(number) << std::endl;
+    double d = 0.0;
+   print_ip(u8);
+   print_ip(u16);
+   print_ip(u32);
+   print_ip(u64);
+   print_ip(str);
+   print_ip(vInt);
+   print_ip(lInt);
+   print_ip(make_tuple);
+   //print_ip(number);  //надоже это тоже работает 
+   print_ip(d); // этот тип игнорируется
+  
+    return 0;
+
+}
